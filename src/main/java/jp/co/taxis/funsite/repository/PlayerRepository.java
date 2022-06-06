a5ff81077cf2e8a0a8f9915d927209c7d7ef72db
User <User@WINDOWS-2VSDDGD>
2022-06-06 10:56:23 +0900

    Repository実装完了

--- src/main/java/jp/co/taxis/funsite/repository/PlayerRepository.java
+++ src/main/java/jp/co/taxis/funsite/repository/PlayerRepository.java
@@ -1,11 +1,18 @@
 package jp.co.taxis.funsite.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import jp.co.taxis.funsite.entity.PlayerEntity;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<PlayerEntity, Integer> {
+
+	@Query(nativeQuery = true, value = "SELECT * FROM player WHERE name LIKE:name")
+	public List<PlayerEntity> playerSearch(@Param("name") String name);
 
 }
--- src/main/java/jp/co/taxis/funsite/repository/SupportMessageRepository.java
+++ src/main/java/jp/co/taxis/funsite/repository/SupportMessageRepository.java
@@ -1,11 +1,19 @@
 package jp.co.taxis.funsite.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import jp.co.taxis.funsite.entity.SupportMessageEntity;
 
 @Repository
-public interface SupportMessageRepository extends JpaRepository<SupportMessageEntity, Integer>{
+public interface SupportMessageRepository extends JpaRepository<SupportMessageEntity, Integer> {
+
+	@Query(nativeQuery = true, value = "SELECT * FROM suport_message WHERE message ORDER BY sendDatetime")
+	public List<SupportMessageEntity> selectByMessage(@Param("sendDatetime") LocalDate sendDatetime);
 
 }
