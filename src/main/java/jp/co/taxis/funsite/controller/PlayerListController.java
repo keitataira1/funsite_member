06a0f806d715a808e2c57265857723676fce1b4c
User <User@WINDOWS-LHAT3L0>
2022-06-07 15:23:34 +0900

    PlayerListControllerの一部修正

--- src/main/java/jp/co/taxis/funsite/controller/PlayerListController.java
+++ src/main/java/jp/co/taxis/funsite/controller/PlayerListController.java
@@ -14,6 +14,7 @@ import jp.co.taxis.funsite.service.PlayerService;
 import jp.co.taxis.funsite.service.TopicService;
 
 @Controller
+@RequestMapping("index")
 public class PlayerListController {
 	
 	@Autowired
