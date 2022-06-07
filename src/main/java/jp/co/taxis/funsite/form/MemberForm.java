7af27ffcc5c9bf5bcc9a0392de16be4df17fa9c8
User <User@WINDOWS-2VSDDGD>
2022-06-07 14:49:15 +0900

    MemberForm再度

--- src/main/java/jp/co/taxis/funsite/form/MemberForm.java
+++ src/main/java/jp/co/taxis/funsite/form/MemberForm.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,15 +17,28 @@ import lombok.NoArgsConstructor;
 public class MemberForm {
 
 	@NotEmpty
-	@Pattern(regexp = ".{1-20}")
+	@Pattern(regexp = ".{1,20}")
 	private String mail;
+
 	@NotEmpty
-	@Pattern(regexp = ".{1-10}")
+	@Pattern(regexp = ".{1,10}")
 	private String password;
+
+	@NotEmpty
+	@Pattern(regexp = ".{1,10}")
 	private String realName;
+
+	@NotEmpty
+	@Pattern(regexp = ".{1,10}")
 	private String memberName;
+
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
 	private LocalDate birthday;
+
+	@Pattern(regexp = ".{1,10}")
 	private Integer postalCode;
+
+	@Pattern(regexp = ".{1,50}")
 	private String address;
 
 }
