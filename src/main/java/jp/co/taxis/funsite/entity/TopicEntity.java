3468ca860419bdceb41fccbb9af91a15c47f73ef
User <User@WINDOWS-LHAT3L0>
2022-06-10 10:57:26 +0900

    TopicEntityの修正委

--- src/main/java/jp/co/taxis/funsite/entity/TopicEntity.java
+++ src/main/java/jp/co/taxis/funsite/entity/TopicEntity.java
@@ -5,8 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Version;
 
@@ -26,8 +24,7 @@ public class TopicEntity {
 	@Column(name = "id")
 	private Integer id;
 	
-	@ManyToOne
-	@JoinColumn(name = "player_id",referencedColumnName="player.name")
+	@Column(name = "player_id")
 	private Integer playerId;
 	
 	@Column(name = "topic")
