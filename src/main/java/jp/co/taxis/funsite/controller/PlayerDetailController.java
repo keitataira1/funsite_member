b6d82f62d859bd12474f1639ef2e3a264411c24f
User <User@WINDOWS-2VSDDGD>
2022-06-10 11:18:37 +0900

    選手詳細情報のControllerとThymeleaf作成済み

--- src/main/java/jp/co/taxis/funsite/controller/PlayerDetailController.java
+++ src/main/java/jp/co/taxis/funsite/controller/PlayerDetailController.java
@@ -3,15 +3,14 @@ package jp.co.taxis.funsite.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jp.co.taxis.funsite.entity.PlayerEntity;
 import jp.co.taxis.funsite.form.PlayerForm;
 import jp.co.taxis.funsite.service.PlayerService;
-import jp.co.taxis.funsite.service.TopicService;
 
 @Controller
 @RequestMapping("player")
@@ -20,20 +19,25 @@ public class PlayerDetailController {
 	@Autowired
 	private PlayerService playerService;
 
-	@Autowired
-	private TopicService topicService;
-
+	/**
+	 * 選手詳細画面表示メソッド
+	 * 
+	 * @param playerForm
+	 * @param result
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value = "player", method = { RequestMethod.POST })
-	public String detail(@ModelAttribute("detail") @Validated PlayerForm playerForm, BindingResult result,
-			Model model) {
+	public String detail(@ModelAttribute("detail") @Validated PlayerForm playerForm, Model model) {
 
-		if (result.hasErrors()) {
-			return "error";
-		}
+		// 選手情報・トピック情報を取得する
+		PlayerEntity player = playerService.selectById("id");
+
+		// 出力
+		model.addAttribute("player", player);
 		
-		//PlayerEntity player=playerService.selectById();
-		
-		return "result";
+		//Viewの選択
+		return "player_detail";
 	}
-	
+
 }
