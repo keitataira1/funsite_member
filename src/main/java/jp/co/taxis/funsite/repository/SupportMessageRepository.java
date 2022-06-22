3c629e4e5dfba7ff7ba3dbd2c541e3ba2064c0f0
User <User@WINDOWS-HP12SIA>
2022-06-22 15:01:01 +0900

    thymeleafの送付

--- src/main/java/jp/co/taxis/funsite/repository/SupportMessageRepository.java
+++ src/main/java/jp/co/taxis/funsite/repository/SupportMessageRepository.java
@@ -18,7 +18,7 @@ public interface SupportMessageRepository extends JpaRepository<SupportMessageEn
 	@Query(nativeQuery = true, value = "SELECT topic_id FROM support_message GROUP BY topic_id ORDER BY COUNT( topic_id ) DESC limit 3")
 	public List<Integer> selectTop3();
 	
-	@Query(nativeQuery = true, value = "SELECT * FROM support_message WHERE topic_id = :id ORDER BY send_datetime DESC")
+	@Query(nativeQuery = true, value = "SELECT * FROM support_message WHERE topic_id = :id ORDER BY send_datetime")
 	public List<SupportMessageEntity> selectTopicMessage(@Param("id") int id);
 }
 
