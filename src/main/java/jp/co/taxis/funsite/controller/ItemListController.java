e947400780a4d305c1a157cf1e311e9193c853a7
User <User@WINDOWS-LHAT3L0>
2022-06-27 09:12:44 +0900

    ItemListの修正

---
+++ src/main/java/jp/co/taxis/funsite/controller/ItemListController.java
@@ -0,0 +1,54 @@
+package jp.co.taxis.funsite.controller;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.co.taxis.funsite.entity.ItemEntity;
+import jp.co.taxis.funsite.form.SearchForm;
+import jp.co.taxis.funsite.service.ItemService;
+
+@Controller
+@RequestMapping("item")
+public class ItemListController {
+
+	@Autowired
+	private ItemService itemService;
+
+	@Autowired
+	private MessageSource messageSource;
+
+	/**
+	 * 商品一覧画面
+	 *
+	 * @param itemForm
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "list", method = { RequestMethod.GET })
+	public String list(@ModelAttribute("search") SearchForm searchForm, BindingResult result, Model model) {
+
+		if (result.hasErrors()) {
+			return "item_list";
+		}
+
+		List<ItemEntity> itemList = itemService.selectAll();
+		if (itemList.isEmpty()) {
+			String message = messageSource.getMessage("list.empty.error", null, Locale.getDefault());
+			model.addAttribute("message", message);
+		}
+		searchForm.getSearchWord();
+		model.addAttribute("itemList", itemList);
+
+		return "item_list";
+	}
+
+}
