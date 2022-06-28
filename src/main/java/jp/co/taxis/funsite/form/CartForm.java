226e6aa1f6494d54e8f30b17c26946cfc267342b
User <User@WINDOWS-2VSDDGD>
2022-06-28 16:10:11 +0900

    cartForm

---
+++ src/main/java/jp/co/taxis/funsite/form/CartForm.java
@@ -0,0 +1,15 @@
+package jp.co.taxis.funsite.form;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartForm {
+	
+	private Integer id;
+	private Integer count;
+
+}
