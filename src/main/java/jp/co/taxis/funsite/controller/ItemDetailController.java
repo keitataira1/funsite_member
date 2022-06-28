d825b083779c6f90684d4d2f7738a53e94f85273
User <User@WINDOWS-2VSDDGD>
2022-06-28 15:13:41 +0900

    itemDetail機能

--- src/main/java/jp/co/taxis/funsite/controller/ItemDetailController.java
+++ src/main/java/jp/co/taxis/funsite/controller/ItemDetailController.java
@@ -14,48 +14,48 @@ import jp.co.taxis.funsite.dto.ItemDto;
 import jp.co.taxis.funsite.dto.UserDto;
 import jp.co.taxis.funsite.entity.ItemEntity;
 import jp.co.taxis.funsite.entity.OrderDetailEntity;
+import jp.co.taxis.funsite.form.CartForm;
 import jp.co.taxis.funsite.service.ItemService;
 
 @Controller
 @RequestMapping("item")
 public class ItemDetailController {
-	
+
 	@Autowired
 	private ItemService itemService;
-	
+
 	@Autowired
 	private ItemDto itemDto;
-	
+
 	@Autowired
 	private UserDto userDto;
-	
+
 	@RequestMapping("detail")
-	public String detail(@RequestParam("id") int id, @RequestParam("message") String message, Model model) {
-		
+	public String detail(@RequestParam("id") int id, @ModelAttribute("cart") CartForm cartItem, Model model) {
+
+		cartItem.setId(id);
 		ItemEntity item = itemService.selectById(id);
-		model.addAttribute(item);
-		model.addAttribute(message);
-		
-		return "item_detail";	
-	
+		model.addAttribute("item", item);
+
+		return "item_detail";
+
 	}
-	
+
 	@RequestMapping("complete")
-	public String complete(@ModelAttribute("count") int count, @ModelAttribute("item") ItemEntity item, RedirectAttributes redirectAttributes) {
-		
+	public String complete(@ModelAttribute("cart") CartForm cartItem, RedirectAttributes redirectAttributes) {
 
 		OrderDetailEntity orderItem = new OrderDetailEntity();
-		
-		orderItem.setItem(itemService.selectById(item.getId()));
+
+		orderItem.setItem(itemService.selectById(cartItem.getId()));
 		orderItem.setMember(userDto.getMemberEntity());
 		orderItem.setOrderDate(LocalDate.now());
-		orderItem.setQuantity(count);
+		//orderItem.setQuantity(cartItem.getCount());
+		orderItem.setQuantity(10);
 		
 		itemDto.setOrderDetailEntity(orderItem);
-		
-		redirectAttributes.addAttribute("id", item.getId());
-		redirectAttributes.addAttribute("message", "カートに保存されました");
-		
+
+		redirectAttributes.addAttribute("id", cartItem.getId());
+
 		return "redirect:detail";
 	}
 
